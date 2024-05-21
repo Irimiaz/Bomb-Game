5ccce320777230958009269697efcd8806b0670b
Irimia Marian <marianirimia27@yahoo.com>
2024-05-21 19:49:15 +0300

    Finished button-module
    Signed-off-by: Irimia Marian <marianirimia27@yahoo.com>

--- code/code.ino
+++ code/code.ino
@@ -1,12 +1,16 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+float maxTime = 100;
 
-//culori_module
+
+//-----------culori_module
 unsigned long startTime;
 bool rgblight = true;
 
 const int buttonPins[3] = {2, 3, 4}; // Push button pins for sequence input
-const int resetButtonPin = 7;        // Reset button pin
-const int redLEDPin = 5;             // Red LED pin
-const int greenLEDPin = 6;           // Green LED pin
+const int resetButtonPin_culori = 7;        // Reset button pin
+const int redLEDPin_culori = 5;             // Red LED pin
+const int greenLEDPin_culori = 6;           // Green LED pin
 const int rgbPins[3] = {9, 10, 11};  // RGB LED pins
 
 int sequence[8];       // Stores the random sequence of 8 colors
@@ -14,30 +18,6 @@ int userSequence[8];   // Stores the user's input sequence
 int userIndex = 0;     // Tracks the user's current input position
 bool userTurn = false;
 
-
-
-void setup() {
-  Serial.begin(9600);
-  for (int i = 0; i < 3; i++) {
-    pinMode(buttonPins[i], INPUT);
-    pinMode(buttonPins[i], INPUT_PULLUP);
-  }
-  pinMode(resetButtonPin, INPUT);
-  pinMode(resetButtonPin, INPUT_PULLUP);
-  pinMode(redLEDPin, OUTPUT);
-  pinMode(greenLEDPin, OUTPUT);
-  for (int i = 0; i < 3; i++) {
-    pinMode(rgbPins[i], OUTPUT);
-  }
-  
-  randomSeed(analogRead(0)); // Seed the random generator
-
-  // Generate a random sequence of 8 colors
-  for (int i = 0; i < 8; i++) {
-    sequence[i] = random(0, 3); // Randomly choose 0 (red), 1 (green), or 2 (blue)
-  }
-
-}
 
 void setColor(int color) {
   switch (color) {
@@ -108,7 +88,7 @@ void colors_module() {
   
   if (userIndex < 8) {
     //reset button
-    if (digitalRead(resetButtonPin) == LOW && buttonPressReset == false && millis() >= buttonTimeNow + period) {
+    if (digitalRead(resetButtonPin_culori) == LOW && buttonPressReset == false && millis() >= buttonTimeNow + period) {
       buttonPressReset = true;
       if(buttonPressReset) {
         Serial.println("Reset button pressed");
@@ -119,7 +99,7 @@ void colors_module() {
         time_now = millis();
       }
     }
-    if (digitalRead(resetButtonPin) == HIGH ) {
+    if (digitalRead(resetButtonPin_culori) == HIGH ) {
       buttonPressReset = false;
     }
   
@@ -159,10 +139,10 @@ void colors_module() {
 
     // Light up the corresponding LED based on the result
     if (!correct) {
-      digitalWrite(greenLEDPin, HIGH);
+      digitalWrite(greenLEDPin_culori, HIGH);
 
     } else {
-      digitalWrite(redLEDPin, HIGH);
+      digitalWrite(redLEDPin_culori, HIGH);
     }
 
     // Reset for the next round
@@ -170,8 +150,84 @@ void colors_module() {
   }
 }
 
-void loop() {
-  colors_module();
+//--------------------buton_module
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int buttonPin_buton = 8;     // Push button pin
+const int greenLEDPin_buton = 13;   // Green LED pin
+const int redLEDPin_buton = 12;     // Red LED pin
+
+unsigned long startTime_buton;     // Start time of the timer
+bool timerRunning = false;   // Timer state
+int seconds = 0;             // Seconds counter
+bool buttonPress_button = false;
+void buton_module() {
+
+  int buttonState = digitalRead(buttonPin_buton);
+  float currentTime = millis() - startTime_buton;  // Calculate elapsed time
+  float seconds = maxTime - currentTime / 1000;
+  // Display the timer on the LCD
+  lcd.setCursor(8, 0);
+  lcd.print(seconds);
+
+  // Check if button is pressed
+  if (buttonState == LOW && buttonPress_button == false) {
+    buttonPress_button = true;
+    // Check if the timer contains the number '2'
+    if (String(seconds).indexOf('2') != -1) {
+      digitalWrite(greenLEDPin_buton, HIGH);
+    } else {
+      digitalWrite(redLEDPin_buton, HIGH);
+    }
+    delay(200);
+  }
+  if (buttonState == HIGH ) {
+    buttonPress_button = false;
+  }
 }
 
+void loop() {
+  colors_module();
+  buton_module();
+}
+
+
+
+void setup() {
+  Serial.begin(9600);
+  digitalWrite(greenLEDPin_buton, LOW);
+  for (int i = 0; i < 3; i++) {
+    pinMode(buttonPins[i], INPUT);
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+  pinMode(resetButtonPin_culori, INPUT);
+  pinMode(resetButtonPin_culori, INPUT_PULLUP);
+  pinMode(redLEDPin_culori, OUTPUT);
+  pinMode(greenLEDPin_culori, OUTPUT);
+  for (int i = 0; i < 3; i++) {
+    pinMode(rgbPins[i], OUTPUT);
+  }
+  
+  randomSeed(analogRead(0)); // Seed the random generator
+
+  // Generate a random sequence of 8 colors
+  for (int i = 0; i < 8; i++) {
+    sequence[i] = random(0, 3); // Randomly choose 0 (red), 1 (green), or 2 (blue)
+  }
+
+
+  //buton module
+  pinMode(buttonPin_buton, INPUT);
+  pinMode(greenLEDPin_buton, OUTPUT);
+  pinMode(redLEDPin_buton, OUTPUT);
+  pinMode(buttonPin_buton, INPUT_PULLUP);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(1, 0);
+  lcd.print("Time: ");
+  if (!timerRunning) {
+      startTime_buton = millis();  // Start the timer
+      timerRunning = true;
+    }
+}
 
